b40da6dd53499db64e3c10406042e89d377d9c30
kirito <1970201491@qq.com>
2024-06-08 00:14:32 +0800

    feat:配置全局样式

--- vite.config.ts
+++ vite.config.ts
@@ -20,5 +20,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      scss: {
+        // 引入 mixin.scss 这样就可以在全局中使用 mixin.scss中预定义的变量了
+        // 给导入的路径最后加上 ; 
+        // javascriptEnabled:true,
+        additionalData: '@import "@/styles/global.scss";'
+      }
+    }
+  }
 })
